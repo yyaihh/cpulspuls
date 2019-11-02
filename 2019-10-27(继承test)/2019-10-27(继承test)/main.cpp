031717abc68d5de45cf83d0c602131f44003c7d1
YYaiHH <1903870154@qq.com>
2019-11-02 20:05:38 +0800

    提交

--- 2019-10-27(继承test)/2019-10-27(继承test)/main.cpp
+++ 2019-10-27(继承test)/2019-10-27(继承test)/main.cpp
@@ -1,56 +1,54 @@
-#include<iostream>
-using namespace std;
-class Base
-{
-public:
-	virtual void Func1()
-	{
-		cout << "Base::Func1()" << endl;
-	}
-	virtual void Func2()
-	{
-		cout << "Base::Func2()" << endl;
-	}
-	void Func3()
-	{
-		cout << "Base::Func3()" << endl;
-	}
-	void* operator&(Base& d) {
-		return (void*)this->Func1;
-	}
-private:
-	int _b = 1;
-};
-class Derive : public Base
-{
-public:
-	virtual void Func1()
-	{
-		cout << "Derive::Func1()" << endl;
-	}
-	virtual void Func4()
-	{
-		cout << "Derive::Func4()" << endl;
-	}
-private:
-	int _d = 2;
-};
-class C : public Derive
-{
-public:
-	virtual void Func4()
-	{
-		cout << "C::Func4()" << endl;
-	}
-private:
-	int _d = 2;
-};
-int main()
-{
-	Base b;
-	Derive d;
-	void* f = &b;
-	C c;
-	system("pause");
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//#include<string>
+//class Base
+//{
+//public:
+//	virtual void Func1()
+//	{
+//		cout << "Base::Func1()" << endl;
+//	}
+//	virtual void Func2()
+//	{
+//		cout << "Base::Func2()" << endl;
+//	}
+//	void Func3()
+//	{
+//		cout << "Base::Func3()" << endl;
+//	}
+//private:
+//	int _b = 1;
+//};
+//class Derive : public Base
+//{
+//public:
+//	virtual void Func1()
+//	{
+//		cout << "Derive::Func1()" << endl;
+//	}
+//	virtual void Func4()
+//	{
+//		cout << "Derive::Func4()" << endl;
+//	}
+//private:
+//	int _d = 2;
+//};
+//class C : public Derive
+//{
+//public:
+//	virtual void Func4()
+//	{
+//		cout << "C::Func4()" << endl;
+//	}
+//private:
+//	int _d = 2;
+//};
+//int main_()
+//{
+//	Base b;
+//	Derive d;
+//	C c;
+//	string s;
+//	system("pause");
+//	return 0;
+//}
