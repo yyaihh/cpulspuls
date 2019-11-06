79805b20ee3737015f52ab63c692efd9bdf0ad97
YYaiHH <1903870154@qq.com>
2019-11-07 00:30:33 +0800

    多态

--- 2019-11-5(多态test)/2019-11-5(多态test)/test.cpp
+++ 2019-11-5(多态test)/2019-11-5(多态test)/test.cpp
@@ -1,46 +1,46 @@
-#include<iostream>
-using namespace std;
-class Person {
-public:
-	virtual void BuyTicket() {
-		cout << "全票\n";
-	}
-};
-class Student :public Person {
-public:
-	virtual void BuyTicket() {
-		cout << "学生半价\n";
-	}
-};
-class Soldier :public Person {
-public:
-	virtual void BuyTicket() {
-		cout << "军人优先购票\n";
-	}
-};
-void Buy(Person& people) {
-	people.BuyTicket();
-}
-void Buy1(Person people) {
-	people.BuyTicket();
-}
-void Buy2(Person* people) {
-	people->BuyTicket();
-}
-int main() {
-	Person p;
-	Student st;
-	Soldier so;
-	Buy(p);
-	Buy(st);
-	Buy(so);
-	Buy1(p);
-	Buy1(st);
-	Buy1(so);
-	Buy2(&p);
-	Buy2(&st);
-	Buy2(&so);
-	cout << sizeof(long) << endl;
-	system("pause");
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//class Person {
+//public:
+//	virtual void BuyTicket() {
+//		cout << "全票\n";
+//	}
+//};
+//class Student :public Person {
+//public:
+//	virtual void BuyTicket() {
+//		cout << "学生半价\n";
+//	}
+//};
+//class Soldier :public Person {
+//public:
+//	virtual void BuyTicket() {
+//		cout << "军人优先购票\n";
+//	}
+//};
+//void Buy(Person& people) {
+//	people.BuyTicket();
+//}
+//void Buy1(Person people) {
+//	people.BuyTicket();
+//}
+//void Buy2(Person* people) {
+//	people->BuyTicket();
+//}
+//int main() {
+//	Person p;
+//	Student st;
+//	Soldier so;
+//	Buy(p);
+//	Buy(st);
+//	Buy(so);
+//	Buy1(p);
+//	Buy1(st);
+//	Buy1(so);
+//	Buy2(&p);
+//	Buy2(&st);
+//	Buy2(&so);
+//	cout << sizeof(long) << endl;
+//	system("pause");
+//	return 0;
+//}
