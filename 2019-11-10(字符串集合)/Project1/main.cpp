502f8aa738cf12fe02b4eb98096197dc9c421867
YYaiHH <1903870154@qq.com>
2019-11-10 13:11:56 +0800

    字符串集合

---
+++ 2019-11-10(字符串集合)/Project1/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+	string fun(string& s) {
+		vector<int>tmp1;
+		vector<int>tmp2;
+		string res;
+		tmp1.resize(26, 0);
+		tmp2.resize(26, 0);
+		for (auto i : s) {
+			if (i >= 'a'&&i <= 'z') {
+				if (tmp1[i - 'a']++ == 0) {
+					res += i;
+				}
+			}
+			else {
+				if (tmp2[i - 'A']++ == 0) {
+					res += i;
+				}
+			}
+		}
+		return res;
+	}
+};
+int main() {
+	string s;
+	cin >> s;
+	Solution f;
+	cout << f.fun(s);
+	system("pause");
+	return 0;
+}
