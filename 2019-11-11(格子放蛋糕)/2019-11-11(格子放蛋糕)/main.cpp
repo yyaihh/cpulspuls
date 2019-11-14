ffe02ea7d417b7c49e810f6bfec9615d57d77710
YYaiHH <1903870154@qq.com>
2019-11-14 11:08:52 +0800

    添加O(1)复杂度算法

--- 2019-11-11(格子放蛋糕)/2019-11-11(格子放蛋糕)/main.cpp
+++ 2019-11-11(格子放蛋糕)/2019-11-11(格子放蛋糕)/main.cpp
@@ -7,13 +7,7 @@ class Solution {
 	int size2;
 	int count;
 	bool f(vector<vector<int>>& v,int i,int j) {
-		if (i + 2 < size1&&v[i + 2][j] != 0) { 
-			return false;
-		}
 		if (i - 2 >= 0 && v[i - 2][j] != 0) { 
-			return false;
-		}
-		if (j + 2 < size2&&v[i][j + 2] != 0) {
 			return false;
 		}
 		if (j - 2 >= 0&&v[i][j - 2] != 0) {
@@ -22,7 +16,7 @@ class Solution {
 		return true;
 	}
 public:
-	int fun(vector<vector<int>>& v) {
+	int fun(vector<vector<int>> v) {//复杂度O(n^2)
 		count = 0;
 		size1 = v.size();
 		size2 = v[0].size();
@@ -36,52 +30,32 @@ public:
 		}
 		return count;
 	}
-	int fun1(int W, int H) {
-		int i, j;
-		if (W >= 4) {
-			i = W % 4;
-		}
-		else {
-			i = 0;
-		}
-		if (H >= 4) {
-			j = H % 4;
-		}
-		else {
-
-		}
+	int fun1(int W, int H) {//复杂度O(1)
+		int i, j, count = 0;
+		i = W % 4;
+		j = H % 4;
+		i = (i ? (4 - i) : 0);//缺几行
+		j = (j ? (4 - j) : 0);//缺几列
 		int _W = W + i;
 		int _H = H + j;
-		int count;
-		if (_W > _H) {
-			count = _H * _H;
+		count = _W * _H / 2;
+		count -= (_W*j / 2 + _H * i / 2);
+		if (i == j) {
+			if (i == 2) {
+				count += 4;
+			}
+			else if (i == 1) { 
+				count += 1;
+			}
+			else if(i == 3){
+				count += 5;
+			}
 		}
-		else if(_W < _H){
-			count = _W * _W;
-		}
-		else {
-			count = _H * _W;
-		}
-		if (i != 0 || j != 0) { 
-			count -= (i * W / 2 + j * H / 2); 
-		}
-		else {
-			
-		}
-		if (i - j == 2 || j - i == 2) {//1,3
+		else if (i == 2 && j == 1 || j == 2 && i == 1) { 
 			count += 2;
 		}
-		else if (i == 2 && j == j) {//2,2
+		else {
 			count += 4;
-		}
-		else if (i == 1 && j == j) {//1,1
-			count += 1;
-		}
-		else if(i - j == 1 || j - i == 1) {//1,2
-			count += 2;
-		}
-		else {
-			count += 5;
 		}
 		return count;
 	}
