a7828f920c8ab7820b7e28d71f5297fae3e2fa95
YYaiHH <1903870154@qq.com>
2019-11-30 13:07:54 +0800

    提交

--- 2019-11-29(AVL树)/2019-11-29(AVL树)/AVLTree.h
+++ 2019-11-29(AVL树)/2019-11-29(AVL树)/AVLTree.h
@@ -9,12 +9,12 @@ class TreeNode {
 	T m_data;
 	TreeNode<T>* m_left;
 	TreeNode<T>* m_right;
-	TreeNode<T>* m_pre;
+	TreeNode<T>* m_parent;
 	int m_bf;
 public:
 	TreeNode(const T& data = T())
 		:m_data(data), m_left(nullptr), m_bf(0),
-		m_right(nullptr), m_pre(nullptr)
+		m_right(nullptr), m_parent(nullptr)
 	{}
 	template <class T>
 	friend class AVLTtree;
@@ -30,16 +30,106 @@ class AVLTtree {
 		}
 	}
 	void lRound(TreeNode<T>* pre) {
-
+		TreeNode<T>* parent = pre->m_parent;
+		TreeNode<T>* cur = pre->m_right;
+		cur->m_parent = parent;//旋转节点的父节点变为上上个节点
+		//cur上牵手
+		if (parent) {//parent不是整棵树根节点
+			//parent下牵手
+			if (parent->m_left == pre) {//pre是parent的左孩子
+				/*		1  parent类似于这种情况
+					2 pre
+						3 cur
+							4*/
+				parent->m_left = cur;
+			}
+			else {
+				/*1parent类似于这种
+					 2 pre
+						 3 cur
+							 4*/
+				parent->m_right = cur;
+			}
+		}
+		else {//parent是整棵树根节点
+			m_root = cur;
+		}
+		pre->m_right = cur->m_left;//pre下牵手
+		if (cur->m_left) {
+			/*1 parent                 
+	      0.5	2 pre
+	   0.25	  1.5  3 cur       --->    
+		       cur->l	4
+							5 newp*/
+			cur->m_left->m_parent = pre;
+		}
+		cur->m_left = pre;//cur下牵手
+		pre->m_parent = cur;//pre上牵手
+		cur->m_bf = pre->m_bf = 0;//此时cur和pre平衡因子必为0
+	}
+	void rRound(TreeNode<T>* pre) {
+		TreeNode<T>* parent = pre->m_parent;
+		TreeNode<T>* cur = pre->m_left;
+		cur->m_parent = parent;//旋转节点的父节点变为上上个节点
+		//cur上牵手
+		if (parent) {//parent不是整棵树根节点
+			//parent下牵手
+			if (parent->m_right == pre) {//pre是parent的右孩子
+				/*		1  parent类似于这种情况
+					        4 pre
+						3 cur
+					2*/
+				parent->m_right = cur;
+			}
+			else {
+				/*          4parent类似于这种
+					    3 pre
+					2 cur
+				1*/
+				parent->m_left = cur;
+			}
+		}
+		else {//parent是整棵树根节点
+			m_root = cur;
+		}
+		pre->m_left = cur->m_right;//pre下牵手
+		if (cur->m_right) {
+			/*   6 parent
+		    pre2	2
+		 cur3  2.5     7
+	     4   3.5            
+      5 newp    */
+			cur->m_right->m_parent = pre;
+		}
+		cur->m_right = pre;//cur下牵手
+		pre->m_parent = cur;//pre上牵手
+		cur->m_bf = pre->m_bf = 0;//此时cur和pre平衡因子必为0
 	}
 	void rlRound(TreeNode<T>* pre) {
-
-	}
-	void lRound(TreeNode<T>* pre) {
-
+		TreeNode<T> * right = pre->m_right;
+		TreeNode<T> * newroot = right->m_left;
+		int flag = newroot->m_bf;
+		rRound(pre->m_right);
+		lRound(pre);
+		if (flag == -1) {
+			right->m_bf = 1;
+		}
+		else {
+			pre->m_bf = -1;
+		}
 	}
 	void lrRound(TreeNode<T>* pre) {
-
+		TreeNode<T> * left = pre->m_left;
+		TreeNode<T> * newroot = left->m_right;
+		int flag = newroot->m_bf;
+		lRound(pre->m_left);
+		rRound(pre);
+		if (flag == -1) {
+			pre->m_bf = 1;
+		}
+		else {
+			left->m_bf = -1;
+		}
 	}
 public:
 	AVLTtree()
@@ -77,7 +167,7 @@ public:
 		else {
 			pre->m_left = newp;
 		}
-		newp->m_pre = pre;//往前也接上
+		newp->m_parent = pre;//往前也接上
 		TreeNode<T>* cur = newp;
 		while (pre) {
 			if (pre->m_left == newp) {//新节点是左子树
@@ -93,7 +183,7 @@ public:
 				if (pre->m_buf == -1 || pre->m_buf == 1) {
 					//若平衡因子满足条件, 继续向上判断
 					cur = pre;
-					pre = pre->m_pre;
+					pre = pre->m_parent;
 				}//else不平衡了, 所以要break
 				else if (pre->m_bf == 2) { 
 					if (cur->m_bf == 1) {//根右右
@@ -106,7 +196,7 @@ public:
 				}
 				else {//m_bf==-2
 					if (cur->m_bf == -1) {//根左左
-
+						rRound(pre);
 					}
 					else {//根右左
 						rRound(pre);
